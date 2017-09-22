f2e9cbce923b44249896ac9d4087bcf79302e9e9
sbcreates <drawsarahb@gmail.com>
2017-09-22 14:48:28 -0400

    Finished writing tests for temperature conversions. tested they run

--- temperature.rb
+++ temperature.rb
@@ -4,7 +4,25 @@ class Temperature
   end
 
   def to_fahrenheit
-    
+    temp = @temp.values[0]
+    temp_type = @temp.keys[0]
+    if temp_type != :f
+      new_temp = (temp * 9) / 5 + 32
+      return new_temp
+    else
+      temp
+    end
+  end
+
+  def to_celsius
+    temp = @temp.values[0]
+    temp_type = @temp.keys[0]
+    if temp_type != :c
+      new_temp = ((temp - 32) * 5) / 9
+      return new_temp
+    else
+      temp
+    end
   end
 
 end
